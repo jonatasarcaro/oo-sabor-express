c609078d2547b45912571e67a4d62cb55b7a9aeb
jonatasarcaro <jonatasarcaro943@gmail.com>
2025-03-21 21:24:29 -0300

    Começando POO

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,3 +1,35 @@
-nome
-categoria
-ativo
+class Restaurante:
+    restaurantes = []
+    
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self._nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        print(f'{"Nome do Restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome.title()
+    
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
+
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca._nome = 'Praça 2.0'
+restaurante_pizza = Restaurante('pizza Express', 'Italiana')
+
+Restaurante.listar_restaurantes()
