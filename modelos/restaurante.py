aef6a59a991638d8f32d1334399f04ee4f6732b8
jonatasarcaro <jonatasarcaro943@gmail.com>
2025-03-24 19:37:41 -0300

    Arrumando @property

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,35 +1,30 @@
 class Restaurante:
     restaurantes = []
-    
+
     def __init__(self, nome, categoria):
         self._nome = nome.title()
-        self.categoria = categoria.upper()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
-
+    
     def __str__(self):
-        return f'{self._nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
-        print(f'{"Nome do Restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
-    
-    @property
-    def nome(self):
-        return self._nome
-    
-    @nome.setter
-    def nome(self, nome):
-        self._nome = nome.title()
-    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐'
-
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca._nome = 'Praça 2.0'
-restaurante_pizza = Restaurante('pizza Express', 'Italiana')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 Restaurante.listar_restaurantes()
