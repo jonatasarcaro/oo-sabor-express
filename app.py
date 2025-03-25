5b04ade7638ca05767bc84391f207882be176581
jonatasarcaro <jonatasarcaro943@gmail.com>
2025-03-24 23:03:08 -0300

    Comentando codigo antigo

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+#restaurante_praca.receber_avaliacao('Gui', 10)
+#restaurante_praca.receber_avaliacao('Lais', 8)
+#restaurante_praca.receber_avaliacao('Emy', 2)
 
 def main():
-    Restaurante.listar_restaurantes()
+    #Restaurante.listar_restaurantes()
+    pass
 
 if __name__ == '__main__':
     main()
