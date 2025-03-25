cebb1fb850c2df5972196f5569c7c86391df9e5e
jonatasarcaro <jonatasarcaro943@gmail.com>
2025-03-24 21:41:30 -0300

    Arrumando o  @property e adicionando a coluna nota

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from modelos.restaurante import Restaurante
 restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_praca.receber_avaliacao('Gui', 10)
 restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 5)
+restaurante_praca.receber_avaliacao('Emy', 2)
 
 def main():
     Restaurante.listar_restaurantes()
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     main()
