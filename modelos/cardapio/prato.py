e2b023a5d5a5ee65c2d79ce5acf4a66492302721
jonatasarcaro <jonatasarcaro943@gmail.com>
2025-03-24 22:40:57 -0300

    Criando herenças e cardapios

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,3 +1,7 @@
-class Prato:
-    def __init__(self,nome,preco,descricao):
-        pass
+from modelos.cardapio.item_cardapio import ItemCardapio
+
+class Prato(ItemCardapio):  # A classe 'Prato' herda de 'ItemCardapio', vai usar os atributos dessa classa
+        def __init__(self,nome,preco,descricao):
+             super().__init__(nome,preco) # Minha classe prato ta usando os atributos e informações da classe principal
+             self.descricao = descricao # Vamos usar as informações da classa mãe (nome,preco) porém podemos adicionar mais quando for necessário
+             
